0a159049598f22dbf6cd3c459791f22fa61991cc
vsevolodgornostaev <sevlantiy@inbox.ru>
2022-02-23 10:00:20 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Vsevolod Gornostaev");
         System.out.println("26.04.1987");
     }
