c78ea7fe11ef1476182939d60105e2135f408687
vsevolodgornostaev <sevlantiy@inbox.ru>
2022-03-19 13:18:34 +0300

    5.0.1. Сумма чисел. [#226416 #277040]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -15,14 +15,4 @@ public class Counter {
         System.out.println(sum(1, 1));
 
     }
-
-    public static int sumByEven(int start, int finish) {
-        int sum = 0;
-        for (int i = start; i <= finish; i++) {
-            if (i % 2 == 0) {
-                sum = sum + i;
-            }
-        }
-        return sum;
-    }
 }
