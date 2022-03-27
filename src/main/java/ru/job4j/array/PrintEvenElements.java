8e4c970280080b1855d4241216c2ef361ad760b4
vsevolodgornostaev <sevlantiy@inbox.ru>
2022-03-27 18:38:59 +0300

    6.4.1.1. Обход массива с последнего элемента [#361619]

---
+++ src/main/java/ru/job4j/array/PrintEvenElements.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class PrintEvenElements {
+    public static String printEvenInts(int[] numbers) {
+        String ln = System.lineSeparator();
+        String res = "Элементы массива с чётными индексами, начиная с последнего: " + ln;
+        for (int i = 0; i < numbers.length; i++) {
+            if ((numbers.length - 1 - i) % 2 == 0) {
+                res += numbers[numbers.length - 1 - i] + ln;
+            }
+        }
+        return res;
+    }
+}
---
+++ src/test/java/ru/job4j/array/PrintEvenElementsTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PrintEvenElementsTest {
+    @Test
+    public void whenArrayHasOddNumberOfElements() {
+        String ln = System.lineSeparator();
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        String expected = "Элементы массива с чётными индексами, начиная с последнего: " + ln
+                + 15 + ln + 13 + ln + 11 + ln + 9 + ln + 7 + ln + 5 + ln + 3 + ln + 1 + ln;
+        assertEquals(expected, PrintEvenElements.printEvenInts(numbers));
+    }
+
+    @Test
+    public void whenArrayHasEvenNumberOfElements() {
+        String ln = System.lineSeparator();
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        String expected = "Элементы массива с чётными индексами, начиная с последнего: " + ln
+                + 13 + ln + 11 + ln + 9 + ln + 7 + ln + 5 + ln + 3 + ln + 1 + ln;
+        assertEquals(expected, PrintEvenElements.printEvenInts(numbers));
+    }
+}
